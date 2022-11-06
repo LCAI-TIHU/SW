f05264715881c8474dce54085ccf2ac88435c997
jiazhaorong <jiazhaorong@inspur.com>
2022-11-06 01:11:49 -0400

    Modify README

--- firmware/README.md
+++ firmware/README.md
@@ -2,7 +2,7 @@ TIHU FIRMWARE
 # Introduction
 TIHU runs in barematel mode, and is driven by firmware to complete computing tasks submitted by RUNTIME. TIHU is comprised of RISC-V cpu, nvdla, NoC bus, PCIe module, DDR, SRAM, bootROM, DMA and peripherals.
 
-The workflow of TIHU is:
+The workflow of TIHU is:  
     * Initialize CPU clock, serial, timer and interrupt;    
     * Wait for computing tasks submitted by RUNTIME;  
     * RUNTIME submits task-list and address-list to TIHU, and raise PCIe interrupt to CPU;  
--- xdma/tests/download_firmware.sh
+++ xdma/tests/download_firmware.sh
@@ -1,3 +1,21 @@
 #! /bin/sh
-su root
-source /home/jiazr/linux-kernel/linux-kernel/tests/debug.sh
+
+sudo su <<EOF
+pwd
+
+echo "Insmod xdma.ko."
+insmod ../xdma/xdma.ko
+
+echo "Download firmware."
+## copy firmware 
+cp ../../firmware/src/debug/firmware.bin ./
+
+## download to ram
+# python3 download_firmware.py -i firmware.bin -a 0x40640000 -e  0x40640000
+# echo "python3 download_firmware.py -i firmware.bin -a 0x40640000 -e  0x40640000"
+
+##download to ddr
+python3 download_firmware.py -i firmware.bin -a 0x80000000 -e  0x80000000
+echo "python3 download_firmware.py -i firmware.bin -a 0x80000000 -e  0x80000000"
+
+EOF
